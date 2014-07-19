37b4b868940298b3ea31710561753d1ff8d98960
cloudfflies <cloudfflies@gmail.com>
2014-07-19 23:28:42 +0700

    Update qldt.py

--- qldt.py
+++ qldt.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+
 import mechanize
 import cookielib
 from pprint import pprint
 import sys
-
+from bs4 import BeautifulSoup
 
 
 #lấy username và pass
@@ -20,7 +21,7 @@ def user_pass():
 def dang_nhap(US,PS):
 	global br
 	#login
-	br.open('https://qldt2.neu.edu.vn/cmcsoft.iu.web.info/Login.aspx')
+	br.open(qldt+'cmcsoft.iu.web.info/Login.aspx')
 	print ("Đang chạy 1/2")
 	br.select_form(nr = 0)
 	br.form['txtUserName'] = US
@@ -35,31 +36,80 @@ def dang_nhap(US,PS):
 	else:
 		print('Đăng nhập không thành công, kiểm tra lại mật khẩu và tài khoản')
 
-def get_value(string,location):
-	string = string[location:]
-	location = string.find(">") + 1
-	end = string.find("<")
-	return string[location:end]
 	
 #load trang thư mục điểm	
 def tree_group():
 	global br
-	global data
 	#load Thu muc diem
-	br.open('https://qldt2.neu.edu.vn/CMCSoft.IU.Web.Info/CourseByFieldTree.aspx') 
+	br.open(qldt+'CMCSoft.IU.Web.Info/CourseByFieldTree.aspx') 
 	br.select_form(nr = 0)
 	#Chọn Kiểm tra tình trạng hoàn thành chương trình học
 	br['CTDT'] = ['rdoCheckPassedStageBystu']
 	resp = br.submit()
 	#
-	CreditGroup = []
 	resp = resp.read()
-	data = data.write(resp)
+	group_sub(resp)
+	
+#xửa lý trang thư mục điểm 
+def group_sub(resp):
+	global data
+	global br
+	resp = BeautifulSoup(resp)
+	#tên sinh viên
+	UserFullName = resp.find(id ='PageHeader1_lblUserFullName') 
+	UserFullName = unicode(UserFullName.string)
 
+	#StageID
+	StageId = resp.find(id ='hidStageId')
+	StageId = unicode(StageId['value'])
+
+	#StudentID
+	StudentId = resp.find(id ='hidStudentId')
+	StudentId = unicode(StudentId['value'])
+
+	#tên nghành 
+	lblTitle = resp.find(id ='lblTitle')
+	lblTitle = unicode(lblTitle.contents[2].string)
+
+	#tên group
+	k = resp.find_all(attrs={"onclick":"return OpenPopupList(this);"})
+
+	GroupSubjectName = []
+	for line in k:
+		GroupSubjectName.append(unicode(line.string))
+	
+	#group id
+	GroupSubjectId = []
+	for i in range(2,30):
+		k = 'grdField__ctl'+str(i)+'_hidCreditGroupSubjectId'
+		try:
+			k = str(resp.find_all(id = k)[0]['value'])
+			GroupSubjectId.append(k)
+		except:
+			break
+
+	for GSI in GroupSubjectId:
+		address = qldt+'CMCSoft.IU.Web.Info/PopupList/GroupCourseMarkDetail.aspx?CreditGroupId='+GSI+'&StudentId='+StudentId+'&StageId='+StageId
+		resp = br.open(address).read()
+		sub(resp)
+		
+def sub(resp):
+	resp = BeautifulSoup(resp)
+	for i in range(2,30):
+		k = 'grdField__ctl'+str(i)+'_'
+		try:
+			CourseCode = str(resp.find(id = k+'lblCourseCode').string)
+			CourseName = unicode(resp.find(id = k+'lblCourseName').string)
+			CourseCredit = str(resp.find(id = k+'lblCourseCredit').string)
+			Theory = str(resp.find(id = k+'txtTheory').string)
+			print CourseCode,CourseName,CourseCredit,Theory
+		except:
+			break
 	
 #lấy username và pass	
 usname = ""
 uspass = ""
+qldt = "https://"+"qldt2"+".neu.edu.vn/"
 user_pass()
 #mở file data 
 data = open("data.txt","w")
